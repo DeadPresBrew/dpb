fe8599988680a58d523b6d2ee0eca53d5daecaa7
rtravers <rtravers@serlio.com>
2015-01-30 19:43:28 -0600

    Create head.php

---
+++ head.php
@@ -0,0 +1,15 @@
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" type="image/ico" href="/dart_favicon.ico"/>
+	<title>DPB</title>
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+		$(function () {
+			$('.dropdown-toggle').dropdown();
+		});
+	</script>
+
+</head>
